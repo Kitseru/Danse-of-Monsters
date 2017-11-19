bea6b550f6d1520c05a6e7d1c81acc214584e788
Kitseru <33801288+Kitseru@users.noreply.github.com>
2017-11-19 11:06:20 +0100

    Commentaires sur le script menufin

--- Assets/menufin.cs
+++ Assets/menufin.cs
@@ -3,15 +3,15 @@ using System.Collections;
 
 public class menufin : MonoBehaviour
 {
+    // Charge la scene principale
     public void Commencer()
     {
         Application.LoadLevel("Dance");
     }
 
+    // Charge la scene du menu
     public void Menu()
     {
         Application.LoadLevel("Menu");
     }
-
-
 }
